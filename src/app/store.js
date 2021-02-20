67e7547949841a520fb150381ce30ceef68e9cd5
jwang0110 <johnw7@uci.edu>
2021-02-19 21:32:52 -0800

    deleted template files

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import flashcardReducer from "../features/flashcards/flashcardsSlice";
 
 export default configureStore({
 	reducer: {
-		counter: counterReducer,
 		flashcards: flashcardReducer,
 	},
 });
