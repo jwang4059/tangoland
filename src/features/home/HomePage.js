92c6a4ddce01c78eb445d0eb045b04a15980f18a
jwang0110 <johnw7@uci.edu>
2021-02-28 21:23:53 -0800

    updated home page

--- src/features/home/HomePage.js
+++ src/features/home/HomePage.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		backgroundColor: theme.palette.background.paper,
+		borderRadius: ".25rem",
 		padding: theme.spacing(2),
 		margin: theme.spacing(4, 0),
+	},
+	title: {
+		fontSize: "1.125rem",
+		fontWeight: "700",
+	},
+	button: {
+		margin: theme.spacing(2, 0),
 	},
 }));
 
@@ -15,16 +25,20 @@ export const HomePage = () => {
 
 	return (
 		<section className={classes.root}>
-			<Typography variant="h2" component="h2">
+			<Typography className={classes.title} variant="h2" component="h2">
 				Introduction
 			</Typography>
 			<hr />
-			<Typography>
-				Welcome to Tangoland, aka <strong>単語のランド</strong>. The purpose of
-				this app is to test your knowledge on common japanese expressions. To
-				begin, click the Study button now and start your journey through the
-				land of vocab.
+			<Typography gutterBottom>
+				Welcome to Tangoland, aka 単語のランド. The purpose of this app is to
+				test your knowledge on common japanese expressions. To begin, click the
+				Study button now and start your journey through the land of vocab.
 			</Typography>
+			<Link to="/study">
+				<Button className={classes.button} variant="contained" color="primary">
+					Study
+				</Button>
+			</Link>
 		</section>
 	);
 };
--- src/features/layout/Header.js
+++ src/features/layout/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
 	header: {
 		margin: theme.spacing(2),
+	},
+	link: {
+		color: "black",
+		textDecoration: "none",
 	},
 	title: {
 		[theme.breakpoints.down("sm")]: {
@@ -21,9 +26,11 @@ export const Header = () => {
 
 	return (
 		<header className={classes.header}>
-			<Typography className={classes.title} component="h1" variant="h1">
-				TangoLand
-			</Typography>
+			<Link to="/" className={classes.link}>
+				<Typography className={classes.title} component="h1" variant="h1">
+					TangoLand
+				</Typography>
+			</Link>
 		</header>
 	);
 };
