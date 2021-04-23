00ba2569ac5acfa983d929d2eb57525106c7f276
jwang0110 <johnw7@uci.edu>
2021-04-22 17:35:36 -0700

    delete service worker

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
-
 import { fetchFlashcards } from "./features/flashcards/flashcardsSlice";
 
 store.dispatch(fetchFlashcards());
@@ -21,8 +19,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
